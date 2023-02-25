7793e97a757f5a78652dbf800953db9bdc5256ed
Vinh Doan-The <vinhdt@teky.edu.vn>
2023-02-25 10:02:08 +0700

    Add seed data: seeding default users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+# Seed the database with some users
+User.create(
+  email: "doanthevinh1991@gmail.com",
+  password: "123456a@",
+)
